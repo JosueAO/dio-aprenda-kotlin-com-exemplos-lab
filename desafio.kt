051360806259675513bbf5d3ce04f12ed91b6bdd
Josué Alves de Oliveira <31595648+JosueAO@users.noreply.github.com>
2023-03-24 19:05:43 -0300

    Mudanças descritas do README.md

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,81 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel {
+    BASICO, INTERMEDIARIO, AVANCADO
+}
 
-class Usuario
+data class Usuario(val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(
+    val nome: String,
+    val nivel: Nivel,
+    val duracao: Int = 60
+)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
+class Formacao(
+    val nome: String,
+    val nivel: Nivel,
+    vararg conteudos: ConteudoEducacional
+) {
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    val conteudos = mutableListOf(*conteudos)
+
+    fun matricular(vararg usuarios: Usuario) {
+        usuarios.forEach { usuario ->
+            when {
+                inscritos.contains(usuario) -> println("Usuário ${usuario.nome} já está matriculado na formação $nome.")
+                else -> {
+                    inscritos.add(usuario)
+                    println("Usuário ${usuario.nome} matriculado na formação $nome!")
+                }
+            }
+        }
+    }
+
+    fun cancelarMatricula(usuario: Usuario) {
+        if (inscritos.contains(usuario)) {
+            inscritos.remove(usuario)
+            println("Matrícula do usuário ${usuario.nome} cancelada na formação $nome.")
+        } else {
+            println("Usuário ${usuario.nome} não está matriculado na formação $nome.")
+        }
+    }
+
+    fun listarInscritos() {
+        println("Inscritos na formação $nome:")
+        inscritos.forEach { println("- ${it.nome}") }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+    val usuario3 = Usuario("Pedro")
+
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", Nivel.BASICO)
+    val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos", Nivel.INTERMEDIARIO)
+    val conteudo3 = ConteudoEducacional("Desenvolvimento de APIs RESTful", Nivel.INTERMEDIARIO)
+    val conteudo4 = ConteudoEducacional("Testes automatizados", Nivel.AVANCADO)
+
+    val formacao1 = Formacao("Formação Kotlin", Nivel.INTERMEDIARIO, conteudo1, conteudo2, conteudo3)
+    val formacao2 = Formacao("Formação Testes", Nivel.AVANCADO, conteudo4)
+
+    // Vincula conteúdos à formação
+    formacao1.conteudos.addAll(listOf(conteudo1, conteudo2, conteudo3))
+    formacao2.conteudos.add(conteudo4)
+
+    // Matricula usuários nas formações
+    formacao1.matricular(usuario1, usuario2)
+    formacao2.matricular(usuario3)
+
+    // Exibe lista de inscritos nas formações
+    formacao1.listarInscritos() // Saída esperada: João, Maria
+    formacao2.listarInscritos() // Saída esperada: Pedro
+
+    // Tenta matricular usuário já inscrito em uma formação
+    formacao1.matricular(usuario1) // Saída esperada: Usuário João já está matriculado na formação Formação Kotlin.
+
+    // Cancela matrícula de usuário
+    formacao1.cancelarMatricula(usuario2) // Saída esperada: Matrícula do usuário Maria cancelada na formação Formação Kotlin.
+    formacao1.listarInscritos() // Saída esperada: João
 }
